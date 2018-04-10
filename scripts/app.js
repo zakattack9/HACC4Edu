166edf7245a4a5403a2d7c46bb2116bad9fb81b1
AOE-Dylan <dylantt2000@gmail.com>
2018-04-10 12:42:03 -1000

    starting map

---
+++ scripts/app.js
@@ -0,0 +1,16 @@
+console.log("hello");
+
+var map;
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        },
+        zoom: 10,
+        mapTypeId : "satellite",
+        disableDefaultUI: true,
+        gestureHandling: 'none'
+    });
+};
